e5662b1849b99afc58897c93c8a56984339f9a1e
usmankhanu9090 <Rasenboun@protonmail.com>
2025-05-11 15:07:55 +0700

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -5,7 +5,7 @@ import string
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
-    return password
+    return password string
 
 # Main function to prompt the user for password length and generate a password
 def main():
